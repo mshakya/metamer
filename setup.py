ecce31d399a2fcaca16eb7a58905de4fb4da4801
Migun Shakya <microbeatic@gmail.com>
2019-06-11 15:48:25 -0600

    hrrl

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ setup(
     author_email='migun@lanl.gov',
     packages=['metamash'],
     scripts=['bin/metamash'],
-    # url='http://pypi.python.org/pypi/TowelStuff/',
+    url='https://github.com/mshakya/metamash',
     license='LICENSE.txt',
     description='comparing metagenomes using mash',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy >= 1.15.1",
+        "scipy >= 1.3.0"
         "luigi >= 2.7.5",
         "plumbum >= 1.6.6",
         "pandas >= 0.23.4",
+        "pathlib >= 1.0.1"
     ],
 )
