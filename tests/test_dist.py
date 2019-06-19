2044f2134ed5752198f49906a01dfc825c7dc9b6
Migun Shakya <microbeatic@gmail.com>
2019-06-18 18:49:32 -0600

    renamed files

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -11,9 +11,7 @@ lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 bin_path = os.path.join(lib_path, 'bin')
 sys.path.append(lib_path)
 os.environ["PATH"] += os.pathsep + bin_path
-from metamash import calc_dist
-from plumbum.cmd import cp
-
+from metamash import sketch, dist
 
 def test_CalculateDist(tmpdir):
     """
@@ -23,11 +21,14 @@ def test_CalculateDist(tmpdir):
 
     """
 
-    luigi.interface.build([calc_dist.CalculateDist(sk1="tests/data/test_SRS104275_1.fastq.msh",
-                                                   sk2="tests/data/test_SRS144382_1.fastq.gz.msh",
-                                                   threads=2,
-                                                   out_file=os.path.join(tmpdir, "test_matrix.txt"))],
+    luigi.interface.build([sketch.AllSketches(data_folder="tests/data/fqs",
+                              kmer=31, threads=2, sketch=100,
+                              seed=2500, min_copy=2, out_dir="tests/sk_test",
+                              mash_tool="mash")],
+                          local_scheduler=True, workers=1)
+    luigi.interface.build([dist.Alldist(data_folder="tests/mds", threads=2,
+                                        out_file="tests/test_table.txt")],
                           local_scheduler=True, workers=1)
 
     file_basenames = [os.path.basename(x) for x in tmpdir.listdir()] 
-    assert 'test_matrix.txt' in file_basenames
+    assert 'tests/test_table.txt' in file_basenames
