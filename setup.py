ad27fcab5863369d887a958d39e3fd352d4ba9eb
Migun Shakya <microbeatic@gmail.com>
2019-06-19 09:37:16 -0600

    now using setup tools

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='metamer',
     version='0.0.0',
     author='Migun Shakya',
     author_email='migun@lanl.gov',
-    packages=['metamer'],
+    packages=find_packages(),
     scripts=['bin/metamer'],
     url='https://github.com/mshakya/metamer',
     license='LICENSE.txt',
     description='a suite that compare metagenomes',
+    keywords="metagenome k-mers minhash",
     long_description=open('README.md').read(),
     install_requires=[
         "numpy >= 1.15.1",
-        "scipy >= 1.3.0"
+        "scipy >= 1.3.0",
         "luigi >= 2.7.5",
         "plumbum >= 1.6.6",
         "pandas >= 0.23.4",
