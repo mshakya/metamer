96ae1f6aa2349b62d2aa51583c9c28048578d4b4
Migun Shakya <microbeatic@gmail.com>
2019-10-04 18:42:31 -0600

    dist test fixed

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -12,7 +12,7 @@ lib_path = os.path.abspath(os.path.join(dir_path, '..'))
 bin_path = os.path.join(lib_path, 'bin')
 sys.path.append(lib_path)
 os.environ["PATH"] += os.pathsep + bin_path
-from metamer import sketch, dist
+from metamer import sketch, dist, faqcs
 
 def test_CalculateDist(tmpdir):
     """
@@ -22,15 +22,21 @@ def test_CalculateDist(tmpdir):
 
     """
 
-    #luigi.interface.build([sketch.AllSketches(data_folder="tests/data/fqs",
-    #                       kmer=31, threads=2, sketch=100,
-    #                       seed=2500, min_copy=2, out_dir="tests/sk_test",
-    #                       mash_tool="mash")],
-    #                      local_scheduler=True, workers=1)
-    # luigi.interface.build([dist.Alldist(data_folder="/Users/migunshakya/projects/exascale/mash_comp/metamer/mashes/fivehun", threads=8,
-                                        # out_table="tests/dist5.txt", mash_tool="mash")],
-                        #   local_scheduler=True, workers=1)
+    luigi.interface.build([faqcs.RunAllQC(fq_folder="tests/data/fqs",
+                                          out_dir="tests/dist_test",
+                                          num_cpus=1,
+                                          faqc_min_L=50,
+                                          n_cutoff=4),
+                          sketch.AllSketches(fq_folder="tests/data/fqs",
+                          kmer=31, threads=2, sketch=100,
+                          seed=2500, min_copy=2, out_dir="tests/dist_test",
+                          mash_tool="mash"),
+                          dist.Alldist(data_folder="tests/dist_test",
+                                       threads=8,
+                                       out_dir="tests/dist_test",
+                                       mash_tool="mash")],
+                                      local_scheduler=True, workers=1)
 
-    file_exist = os.path.exists("tests/test_table.txt")
-    shutil.rmtree("tests/sk_test")
+    file_exist = os.path.exists("tests/dist_test/mash_dist.txt")
+    shutil.rmtree("tests/dist_test")
     assert file_exist is True
